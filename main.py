6bfc3325e33565dc02977cb4904268560dad0022
Michele Tonutti <michetonu@gmail.com>
2020-03-16 19:55:22 +0100

    Cleanup

--- main.py
+++ main.py
@@ -21,14 +21,6 @@ def first_derivative(x, a, c, d):
     return (a * c * np.exp(-c * (x - d))) / (np.exp(-c * (x - d)) + 1)**2
 
 
-def second_derivative(x, a, c, d):
-    return (
-            a *
-            (((2 * c**2 * np.exp(-2 * c * (x - d))) / (np.exp(-c * (x - d)) + 1)**3)
-                - ((c**2 * np.exp(-c * (x - d))) / (np.exp(-c * (x - d)) + 1)**2))
-    )
-
-
 def fit_predict(x, y, f, x_pred=None):
     popt, pcov = opt.curve_fit(f, x, y, maxfev=100000)
     if x_pred is None:
@@ -36,9 +28,9 @@ def fit_predict(x, y, f, x_pred=None):
     return f(x_pred, *popt)
 
 
-def compute_derivative(df, column, x, f, x_pred=None):
+def compute_derivative(df, column, x, f):
     y = np.array([float(x) for x in (df[column] - df[column].shift(1)).bfill().values])
-    return fit_predict(x, y, f, x_pred)
+    return fit_predict(x, y, f)
 
 
 def plot(x, y, ax, title, points=None):
@@ -54,11 +46,24 @@ def plot_projection(x, y, ax):
     sns.lineplot(x, y, ax=ax, linewidth=1, color='red', alpha=0.5)
 
 
+def compute_all_derivatives(df, column, times_pred):
+    df[f'first_dev_{column}'] = compute_derivative(df, f'{column}_fit', times_pred,
+                                                   first_derivative)
+    df[f'second_dev_{column}'] = (
+            df[f'first_dev_{column}'] - df[f'first_dev_{column}'].shift()
+    )
+
+    df[f'third_dev_{column}'] = (
+            df[f'second_dev_{column}'] - df[f'second_dev_{column}'].shift()
+    )
+    return df
+
+
 if __name__ == "__main__":
     max_days = 100
     min_confirmed = 40
 
-    country = 'Italy'
+    country = 'Korea, South'
     data_path = "/Users/michele/Documents/covid19/docs/timeseries.json"
 
     with open(data_path, 'r') as ff:
@@ -77,57 +82,26 @@ if __name__ == "__main__":
 
     y = scale(df['confirmed'].values)
     df['confirmed_fit'] = fit_predict(x, y, logistic)
-    df['first_dev_confirmed'] = compute_derivative(df, 'confirmed_fit', x, first_derivative)
-    df['second_dev_confirmed'] = (
-            df['first_dev_confirmed'] - df['first_dev_confirmed'].shift()
-    )
-
-    df['third_dev_confirmed'] = (
-            df['second_dev_confirmed'] - df['second_dev_confirmed'].shift()
-    )
+    df = compute_all_derivatives(df, 'confirmed', times_pred=x)
 
     df_projected['confirmed_fit'] = fit_predict(x, y, logistic, x_pred=x_future)
-    df_projected['first_dev_confirmed'] = compute_derivative(df_projected, 'confirmed_fit',
-                                                             x_future,
-                                                             first_derivative, x_pred=x_future)
-    df_projected['second_dev_confirmed'] = (
-            df_projected['first_dev_confirmed'] - df_projected['first_dev_confirmed'].shift()
-    )
-
-    df_projected['third_dev_confirmed'] = (
-            df_projected['second_dev_confirmed'] - df_projected['second_dev_confirmed'].shift()
-    )
+    df_projected = compute_all_derivatives(df_projected, 'confirmed', times_pred=x_future)
 
     y = scale(df['deaths'].values)
     df['deaths_fit'] = fit_predict(x, y, logistic)
-    df['first_dev_deaths'] = compute_derivative(df, 'deaths_fit', x, first_derivative)
-    df['second_dev_deaths'] = (
-            df['first_dev_deaths'] - df['first_dev_deaths'].shift()
-    )
-
-    df['third_dev_deaths'] = (
-            df['second_dev_deaths'] - df['second_dev_deaths'].shift()
-    )
+    df = compute_all_derivatives(df, 'deaths', times_pred=x)
 
     df_projected['deaths_fit'] = fit_predict(x, y, logistic, x_pred=x_future)
-    df_projected['first_dev_deaths'] = compute_derivative(df_projected, 'deaths_fit',
-                                                             x_future,
-                                                             first_derivative, x_pred=x_future)
-    df_projected['second_dev_deaths'] = (
-            df_projected['first_dev_deaths'] - df_projected['first_dev_deaths'].shift()
-    )
+    df_projected = compute_all_derivatives(df_projected, 'deaths', times_pred=x_future)
 
-    df_projected['third_dev_deaths'] = (
-            df_projected['second_dev_deaths'] - df_projected['second_dev_deaths'].shift()
-    )
     fig, axs = plt.subplots(4, 2, figsize=(15, 8))
     date = df.index
     date_proj = df_projected.index
 
     plot(date, df['confirmed_fit'], ax=axs[0, 0], points=df['confirmed'], title='confirmed')
-    plot(date, df['first_dev_confirmed'], ax=axs[1, 0], title='first_dev_confirmed')
-    plot(date, df['second_dev_confirmed'], ax=axs[2, 0], title='second_dev_confirmed')
-    plot(date, df['third_dev_confirmed'], ax=axs[3, 0], title='third_dev_confirmed')
+    plot(date, df['first_dev_confirmed'], ax=axs[1, 0], title="f'(x)")
+    plot(date, df['second_dev_confirmed'], ax=axs[2, 0], title="f''(x)")
+    plot(date, df['third_dev_confirmed'], ax=axs[3, 0], title="f'''(x)")
 
     plot_projection(date_proj, df_projected['confirmed_fit'], ax=axs[0, 0])
     plot_projection(date_proj, df_projected['first_dev_confirmed'], ax=axs[1, 0])
@@ -135,9 +109,9 @@ if __name__ == "__main__":
     plot_projection(date_proj, df_projected['third_dev_confirmed'], ax=axs[3, 0])
 
     plot(date, df['deaths_fit'], ax=axs[0, 1], points=df['deaths'], title='deaths')
-    plot(date, df['first_dev_deaths'], ax=axs[1, 1], title='first_dev_deaths')
-    plot(date, df['second_dev_deaths'], ax=axs[2, 1], title='second_dev_deaths')
-    plot(date, df['third_dev_deaths'], ax=axs[3, 1], title='third_dev_deaths')
+    plot(date, df['first_dev_deaths'], ax=axs[1, 1], title="f'(x)")
+    plot(date, df['second_dev_deaths'], ax=axs[2, 1], title="f''(x)")
+    plot(date, df['third_dev_deaths'], ax=axs[3, 1], title="f'''(x)")
 
     plot_projection(date_proj, df_projected['deaths_fit'], ax=axs[0, 1])
     plot_projection(date_proj, df_projected['first_dev_deaths'], ax=axs[1, 1])
@@ -145,7 +119,7 @@ if __name__ == "__main__":
     plot_projection(date_proj, df_projected['third_dev_deaths'], ax=axs[3, 1])
 
     fig.autofmt_xdate()
-    fig.text(0.5, 0.04, f'days since {min_confirmed} confirmed cases', ha='center')
-    plt.suptitle(country)
+    fig.text(0.5, 0.01, f'days since {min_confirmed} confirmed cases', ha='center')
+    plt.suptitle(f"Country: {country}")
 
     plt.show()
