0efd299bcdfcb24c8e81ced7ebe05bb58101da44
Michele Tonutti <michetonu@gmail.com>
2020-03-23 19:45:13 +0100

    Add saving option

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+from os.path import dirname, realpath
+
 DATA_URL = "https://raw.githubusercontent.com/pomber/covid19/master/docs/timeseries.json"
 
 # Number of days to plot
@@ -5,3 +7,5 @@ MAX_DAYS_AHEAD = 100
 
 # Minimum of confirmed cases from which to start plotting
 MIN_CONFIRMED_CASES = 40
+
+SRC_PATH = dirname(realpath(__file__))
--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import os
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -17,7 +19,7 @@ def compute_all_derivatives(df, column, times_pred):
     return df
 
 
-def run_fit_on_single_country(country):
+def run_fit_on_single_country(country, save=False, path=None):
     data = utils.get_json_from_url(config.DATA_URL)
 
     df = pd.DataFrame(data[country])
@@ -47,30 +49,22 @@ def run_fit_on_single_country(country):
     df_projected['deaths_fit'] = utils.fit_predict(x, y, utils.logistic, x_pred=x_future)
     df_projected = compute_all_derivatives(df_projected, 'deaths', times_pred=x_future)
 
-    fig, axs = plt.subplots(4, 2, figsize=(15, 8))
+    fig, axs = plt.subplots(2, 2, figsize=(15, 8))
     x = df.index
     x_proj = df_projected.index
 
     utils.plot(x, df['confirmed_fit'], ax=axs[0, 0], points=df['confirmed'], title='confirmed',
                label='best fit', scatter_label='actual')
     utils.plot(x, df['first_dev_confirmed'], ax=axs[1, 0], title="f'(x)")
-    utils.plot(x, df['second_dev_confirmed'], ax=axs[2, 0], title="f''(x)")
-    utils.plot(x, df['third_dev_confirmed'], ax=axs[3, 0], title="f'''(x)")
 
     utils.plot_projection(x_proj, df_projected['confirmed_fit'], ax=axs[0, 0], label='projected')
     utils.plot_projection(x_proj, df_projected['first_dev_confirmed'], ax=axs[1, 0])
-    utils.plot_projection(x_proj, df_projected['second_dev_confirmed'], ax=axs[2, 0])
-    utils.plot_projection(x_proj, df_projected['third_dev_confirmed'], ax=axs[3, 0])
 
     utils.plot(x, df['deaths_fit'], ax=axs[0, 1], points=df['deaths'], title='deaths')
     utils.plot(x, df['first_dev_deaths'], ax=axs[1, 1], title="f'(x)")
-    utils.plot(x, df['second_dev_deaths'], ax=axs[2, 1], title="f''(x)")
-    utils.plot(x, df['third_dev_deaths'], ax=axs[3, 1], title="f'''(x)")
 
     utils.plot_projection(x_proj, df_projected['deaths_fit'], ax=axs[0, 1])
     utils.plot_projection(x_proj, df_projected['first_dev_deaths'], ax=axs[1, 1])
-    utils.plot_projection(x_proj, df_projected['second_dev_deaths'], ax=axs[2, 1])
-    utils.plot_projection(x_proj, df_projected['third_dev_deaths'], ax=axs[3, 1])
 
     axs[0, 0].legend(loc=(0.8, 0.1))
     fig.autofmt_xdate()
@@ -78,7 +72,12 @@ def run_fit_on_single_country(country):
     plt.suptitle(f"Country: {country}. Last update: {date}")
 
     plt.show()
+    if save:
+        path = path or os.path.join(config.SRC_PATH, f'../examples/{country.lower()}.png')
+        fig.savefig(path, bbox_inches='tight')
+
+    plt.close()
 
 
 if __name__ == "__main__":
-    run_fit_on_single_country('Italy')
+    run_fit_on_single_country('China', save=True)
